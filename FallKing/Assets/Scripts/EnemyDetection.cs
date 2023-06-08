40ca48163e62b2d5ff1a77aa42ce20dc8c364160
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-06-08 00:27:06 -0700

    Refactor the detection code. Working on implementing detection range for flying enemy

--- FallKing/Assets/Scripts/EnemyDetection.cs
+++ FallKing/Assets/Scripts/EnemyDetection.cs
@@ -14,8 +14,15 @@ public class EnemyDetection : MonoBehaviour
         transform.position = physicalBodyPos.position;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        Debug.Log("The layer that involves with the contacts");
+        Collider2D[] res = new Collider2D[10];
+        int numContacts = collision.GetContacts(res);
+        for (int i = 0; i < numContacts; i++)
+        {
+            Debug.Log($"The contact name is {res[i].name}");
+        }
         detectedPlayer = true;
         //Debug.Log("TRIGGER");
     }
