11f60ad17700af081de9263485ef00a944bca87c
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-06-10 01:53:57 -0700

    Got an idea on how to make dark area. Working on sprite mask

---
+++ FallKing/Assets/Scripts/DarkAreaController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkAreaController : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    [Tooltip("Level that use this limited visibility on player")]
+    [SerializeField] GameObject darkLevel;
+
+
+}
